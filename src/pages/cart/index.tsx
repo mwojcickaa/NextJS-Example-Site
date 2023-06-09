ebda7ca7b4720542d51e10e4a610b157faaa58fe
krystian <krystian.zasada@op.pl>
2023-06-09 15:55:25 +0200

    Add CartTable component to page index

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,4 +1,6 @@
 import Header from "@/components/Header/Header"
+import { Box, Typography } from "@mui/material"
+import CartTable from "@/components/CartTable/CartTable"
 import style from './index.module.sass'
 
 export default function Cart() {
@@ -6,7 +8,15 @@ export default function Cart() {
         <>
             <Header />
             <main>
-                cart
+                <Typography
+                    variant="h1"
+                    className={style.order_title}
+                >
+                    Twój koszyk
+                </Typography>
+                <Box className={style.order_panel}>
+                    <CartTable />
+                </Box>
             </main>
         </>
     )
