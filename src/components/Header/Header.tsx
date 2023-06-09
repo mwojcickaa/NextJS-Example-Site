5094a473cfc5d1867feb7fb83bfed66d621ebba7
krystian <krystian.zasada@op.pl>
2023-06-09 14:33:47 +0200

    Create subpage cart

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,45 @@
-import React from "react"
+import Link from 'next/link'
 import { Grid } from "@mui/material"
 import Image from "@/components/Image/Image"
+import useRouterHref from "@/hooks/useRouterHref"
 import CartWithNumber from "./CartWithNumber/CartWithNumber"
 import style from './Header.module.sass'
 
 export default function Header() {
+    const LogoImage = () => (
+        <Image
+            alt="Company logo in header"
+            src="/logo.png"
+        />
+    )
+
     return (
         <Grid
             component="header"
             container
             className={style.grid}
         >
-            <Image
-                alt="Company logo in header"
-                src="/logo.png"
-            />
-            <CartWithNumber />
+            {
+                useRouterHref("/")
+                    ?
+                    <LogoImage />
+                    :
+                    <Link
+                        href="/"
+                        className={style.logo}
+                    >
+                        <LogoImage />
+                    </Link>
+            }
+            {
+                useRouterHref("/cart")
+                    ?
+                    <CartWithNumber />
+                    :
+                    <Link href="/cart">
+                        <CartWithNumber />
+                    </Link>
+            }
         </Grid>
     )
 }
---
+++ src/pages/cart/index.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Header/Header"
+import style from './index.module.sass'
+
+export default function Cart() {
+    return (
+        <>
+            <Header />
+            <main>
+                cart
+            </main>
+        </>
+    )
+}
